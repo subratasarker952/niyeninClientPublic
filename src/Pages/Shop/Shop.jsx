1023317b22662d478dfb4b59a03e05be122580d7
Subrata Sarkar <tccdcc8@gmail.com>
2024-06-21 23:02:17 +0600

    m

--- src/Pages/Shop/Shop.jsx
+++ src/Pages/Shop/Shop.jsx
@@ -90,7 +90,7 @@ const Shop = () => {
               </select>
             </div>
           </div>
-          <div className="grid md:shadow-lg md:grid-cols-3 grid-cols-1 gap-3  text-center">
+          <div className="grid min-h-screen md:shadow-lg md:grid-cols-3 grid-cols-1 gap-3  text-center">
             {products?.map((product) => (
               <div
                 key={product._id}
